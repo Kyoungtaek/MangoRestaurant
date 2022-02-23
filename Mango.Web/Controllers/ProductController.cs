07be26d0ac7473f68d5a42604e8c99498f6bec4b
Kyoungtaek Oh <okts83@gmail.com>
2022-02-23 10:31:00 +0900

    36,37. Product Controller Get All Action

---
+++ Mango.Web/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService service;
+        public ProductController(IProductService service)
+        {
+            this.service = service;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var list = new List<ProductDto>();
+            var response = await service.GetAllProductAsync<ResponseDto>();
+
+            if(response!=null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+
+            return View(list);
+        }
+    }
+}
